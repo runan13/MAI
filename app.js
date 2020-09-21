bbe1fc6e5f7f93ec4174e30f9a56a08e1d44c806
runan13 <65391973+runan13@users.noreply.github.com>
2020-09-21 10:51:53 +0900

    Seperate URLs router

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+
+export default app;
